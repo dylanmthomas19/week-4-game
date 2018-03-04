6be5bc014dc51c4b9986fef34089bdb529c0f428
Dylan Thomas <azelthedemon@gmail.com>
2018-03-04 13:00:13 -0700

    fixed goal/mined bug

--- crystalCollector/assets/javascript/game.js
+++ crystalCollector/assets/javascript/game.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function(){
+//initializing variables
+    var wins = 0;
 
-    var mined=0;
+    var losses = 0;
 
     var goal=Math.floor(Math.random()*100+20);
-    $("#goal").text(goal)
 
     var crystal1 = Math.floor(Math.random()*11+1);
  
@@ -13,10 +14,31 @@ $(document).ready(function(){
     var crystal3 = Math.floor(Math.random()*11+1);
     
     var crystal4 = Math.floor(Math.random()*11+1);
+
+    //printing variables
+    $("#goal").text(goal)
+    
+    $("#wins").text(wins)
+    
+    $("#losses").text(losses)
+    
+    //logging variables
     console.log(crystal1, crystal2, crystal3, crystal4)
-    $("#crystal1").on("click", function(){
-        mined = mined + crystal1
-    })
+
+    //win function
+    function win(){
+        wins++;
+        $("wins").text(wins)
+        reset()
+    }
+
+    //lose function
+    function lose(){
+        losses++;
+        $("losses").text(losses)
+        reset()
+    }
+
     function reset(){
         goal=Math.floor(Math.random()*100+20);
 
@@ -27,10 +49,47 @@ $(document).ready(function(){
         var crystal3 = Math.floor(Math.random()*11+1);
         
         var crystal4 = Math.floor(Math.random()*11+1);
-        
-        mined = 0;
+    
     }
-    $("#crystal1").on("click",function(){
-        $("#crystal1").html("<br>")
+    
+    $("#crystal1").click("img",function(){
+        goal = goal - crystal1;
+        $("#goal").text(goal);
+        if(goal===0){
+            win();
+        }
+        else if(0>goal){
+            lose()
+        }
+    })
+    $("#crystal2").click("img",function(){
+        goal = goal - crystal2
+        $("#goal").text(goal);
+        if(goal===0){
+            win();
+        }
+        else if(0>goal){
+            lose()
+        }
+    })
+    $("#crystal3").click("img",function(){
+        goal = goal - crystal3
+        $("#goal").text(goal);
+        if(goal===0){
+            win();
+        }
+        else if(0>goal){
+            lose()
+        }
+    })
+    $("#crystal4").click("img",function(){
+        goal = goal - crystal4
+        $("#goal").text(goal);
+        if(goal===0){
+            win();
+        }
+        else if(0>goal){
+            lose()
+        }
     })
 })
